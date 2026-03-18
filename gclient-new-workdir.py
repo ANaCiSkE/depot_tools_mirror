8e07900125ba28f056008bded80053b297837aab
Sunny Sachanandani <sunnyps@chromium.org>
2026-03-18 11:59:30 -0700

    gclient-new-workdir: Use git worktree
    Allow using git worktree to link the copied (sub)-repositories back to
    the original. Since we try to use copy-on-write by default, we have to
    mimic some of what `git worktree add` does since it doesn't work well
    when the worktree directory already exists.
    
    This functionality is behind the `--use-git-worktree` flag which is
    disabled by default and there's a `--use-git-symlinks` flag which uses
    the old symlink code path instead of git worktree.
    
    Change-Id: I1bb8bf3ec81a5d1dc923670d66944b466a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/7675410
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>

--- gclient-new-workdir.py
+++ gclient-new-workdir.py
@@ -9,6 +9,7 @@
 
 import argparse
 import os
+import random
 import shutil
 import subprocess
 import sys
@@ -34,6 +35,18 @@ def parse_options():
                         type=os.path.abspath,
                         help='should contain a .gclient file')
     parser.add_argument('new_workdir', help='must not exist')
+    parser.add_argument(
+        '--use-git-worktree',
+        action='store_true',
+        default=False,
+        help='''Use git worktree instead of using symlinks for .git folders.''',
+    )
+    parser.add_argument(
+        '--use-git-symlinks',
+        action='store_false',
+        dest='use_git_worktree',
+        help='''Use symlinks for .git folders instead of using git worktree.''',
+    )
     parser.add_argument(
         '--reflink',
         action='store_true',
@@ -73,7 +86,7 @@ def parse_options():
     return args
 
 
-def get_cp_copy_on_write_flag():
+def cp_copy_on_write_flag():
     return '-c' if sys.platform == 'darwin' else '--reflink'
 
 
@@ -82,7 +95,7 @@ def support_copy_on_write(src, dest):
     assert os.path.isfile(src) and not os.path.islink(src)
     try:
         subprocess.check_output(
-            ['cp', '-a', get_cp_copy_on_write_flag(), src, dest],
+            ['cp', '-a', cp_copy_on_write_flag(), src, dest],
             stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError:
         return False
@@ -98,77 +111,163 @@ def try_btrfs_subvol_snapshot(src, dest):
                               stderr=subprocess.STDOUT)
     except (subprocess.CalledProcessError, OSError):
         return False
+    assert os.path.exists(dest)
     return True
+
+
+def real_git_dir(repo_path):
+    relative_git_dir = (
+        subprocess.check_output(
+            ['git', 'rev-parse', '--git-dir'], cwd=repo_path
+        )
+        .decode()
+        .strip()
+    )
+    return os.path.realpath(os.path.join(repo_path, relative_git_dir))
+
+
+def link_git_repo(src, dest, reflink):
+    print('Linking: %s/.git' % src)
+    src_git_dir = real_git_dir(src)
+    dest_git_dir = os.path.join(dest, '.git')
+    git_common.make_workdir(src_git_dir, dest_git_dir)
+    if reflink:
+        subprocess.check_call(
+            [
+                'cp',
+                '-a',
+                cp_copy_on_write_flag(),
+                os.path.join(src_git_dir, 'index'),
+                os.path.join(dest_git_dir, 'index'),
+            ]
+        )
+    else:
+        print('Running: git checkout -f %s' % dest)
+        subprocess.check_call(['git', 'checkout', '-f'], cwd=dest)
+
+
+def adopt_git_worktree(src, dest):
+    assert os.path.exists(dest)
+    # Rename the existing directory since `git worktree add` won't work if the
+    # worktree directory already exists even with the `--force` flag.
+    tmp_dest = os.path.join(
+        os.path.dirname(dest),
+        'tmp-' + os.path.basename(dest) + '-' + '%08x' % random.getrandbits(32),
+    )
+    os.rename(dest, tmp_dest)
+    # Run `git worktree add --no-checkout` to create the worktree without
+    # touching any files in the worktree.
+    print('Running: git worktree add %s --no-checkout -d -f' % dest)
+    subprocess.check_call(
+        ['git', 'worktree', 'add', dest, '--no-checkout', '-d', '-f'], cwd=src
+    )
+    # Move the .git file from the worktree to the temporary directory and move
+    # the temporary directory back to the worktree directory. Note that the .git
+    # file is different from the worktree's real .git directory used below.
+    shutil.move(os.path.join(dest, '.git'), tmp_dest)
+    shutil.rmtree(dest)
+    os.rename(tmp_dest, dest)
+    # Copy the index so that the worktree is aware of files in the repository.
+    subprocess.check_call(
+        [
+            'cp',
+            '-a',
+            cp_copy_on_write_flag(),
+            os.path.join(real_git_dir(src), 'index'),
+            os.path.join(real_git_dir(dest), 'index'),
+        ]
+    )
+
+
+def create_git_worktree(src, workdir):
+    print('Running: git worktree add %s -d -f' % workdir)
+    subprocess.check_call(
+        ['git', 'worktree', 'add', workdir, '-d', '-f'], cwd=src
+    )
 
 
 def main():
     args = parse_options()
 
-    gclient = os.path.join(args.repository, '.gclient')
-    if os.path.islink(gclient):
-        gclient = os.path.realpath(gclient)
-    new_gclient = os.path.join(args.new_workdir, '.gclient')
-
+    used_btrfs_subvol_snapshot = False
     if try_btrfs_subvol_snapshot(args.repository, args.new_workdir):
         # If btrfs is being used, reflink support is always present, and there's
         # no benefit to not using it.
         args.reflink = True
+        used_btrfs_subvol_snapshot = True
     else:
         os.makedirs(args.new_workdir)
+
+    # If any of the operations below fail, we want to clean up the new workdir.
+    try:
+        gclient = os.path.realpath(os.path.join(args.repository, '.gclient'))
+        new_gclient = os.path.join(args.new_workdir, '.gclient')
+
         if args.reflink is None:
             args.reflink = support_copy_on_write(gclient, new_gclient)
             if args.reflink:
                 print('Copy-on-write support is detected.')
-        os.symlink(gclient, new_gclient)
 
-    if args.reflink and args.link_root_git_repo_only is None:
-        # Since we're doing a btrfs subvolume snapshot or reflink copy, the
-        # sub-repositories will already be present in the copy, and we only need
-        # to deal with linking the .git directory for the root repository.
-        args.link_root_git_repo_only = True
+        if not os.path.exists(new_gclient):
+            os.symlink(gclient, new_gclient)
 
-    for root, dirs, _ in os.walk(args.repository):
-        if '.git' in dirs:
-            workdir = root.replace(args.repository, args.new_workdir, 1)
+        if args.reflink and args.link_root_git_repo_only is None:
+            # Since we're doing a btrfs subvolume snapshot or reflink copy, the
+            # sub-repositories will already be present in the copy, and we only
+            # need to link the .git directory for the root repository.
+            args.link_root_git_repo_only = True
 
-            if args.reflink:
-                if not os.path.exists(workdir):
-                    print('Copying: %s' % workdir)
-                    subprocess.check_call([
-                        'cp', '-a',
-                        get_cp_copy_on_write_flag(), root, workdir
-                    ])
-                shutil.rmtree(os.path.join(workdir, '.git'))
+        for root, dirs, _ in os.walk(args.repository):
+            if '.git' in dirs:
+                workdir = root.replace(args.repository, args.new_workdir, 1)
 
-            print('Linking: %s/.git' % workdir)
-            git_common.make_workdir(os.path.join(root, '.git'),
-                                    os.path.join(workdir, '.git'))
-            if args.reflink:
-                subprocess.check_call([
-                    'cp', '-a',
-                    get_cp_copy_on_write_flag(),
-                    os.path.join(root, '.git', 'index'),
-                    os.path.join(workdir, '.git', 'index')
-                ])
+                if args.reflink:
+                    if not os.path.exists(workdir):
+                        print('Copying: %s' % workdir)
+                        subprocess.check_call(
+                            ['cp', '-a', cp_copy_on_write_flag(), root, workdir]
+                        )
+                    shutil.rmtree(os.path.join(workdir, '.git'))
+
+                if args.use_git_worktree:
+                    if args.reflink:
+                        adopt_git_worktree(root, workdir)
+                    else:
+                        create_git_worktree(root, workdir)
+                else:
+                    link_git_repo(root, workdir, reflink=args.reflink)
+
                 # Break out of the for loop if we're only linking the root git
                 # repository's .git folder and using copy-on-write since all the
-                # sub-directories will already be copied and we are done here.
+                # sub-repositories will already be copied and we are done here.
                 # Otherwise, we can't avoid visiting all sub-repositories for
-                # copying over the .git folder and checkout using git.
-                if args.link_root_git_repo_only:
+                # checking out the files with git.
+                if args.reflink and args.link_root_git_repo_only:
                     break
-            else:
-                print('Running: git checkout -f %s' % workdir)
-                subprocess.check_call(['git', 'checkout', '-f'], cwd=workdir)
 
-    if args.reflink:
-        print(
-            textwrap.dedent('''\
-      The repo was copied using copy-on-write, and the artifacts were retained.
-      More details on http://crbug.com/721585.
+        if args.reflink:
+            print(
+                textwrap.dedent(
+                    '''\
+        The repo was copied using copy-on-write, and the artifacts were retained.
+        More details on http://crbug.com/721585.
 
-      Depending on your usage pattern, you might want to do "gn gen"
-      on the output directories. More details: http://crbug.com/723856.'''))
+        Depending on your usage pattern, you might want to do "gn gen"
+        on the output directories. More details: http://crbug.com/723856.'''
+                )
+            )
+    except Exception as e:
+        print(f'Error: {e}')
+        print(f'Cleaning up {args.new_workdir}')
+        if used_btrfs_subvol_snapshot:
+            subprocess.check_call(
+                ['btrfs', 'subvol', 'delete', args.new_workdir]
+            )
+        else:
+            shutil.rmtree(args.new_workdir, ignore_errors=True)
+        return 1
+
+    return 0
 
 
 if __name__ == '__main__':
