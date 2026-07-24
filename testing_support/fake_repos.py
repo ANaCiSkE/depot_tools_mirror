f394ab2c993283e94680ca13db98b99927868e98
Mike Frysinger <vapier@chromium.org>
2026-07-24 02:30:19 -0700

    Update license boilerplate text in source code files
    Normally we don't do this, but enough changes have accumulated that
    we're doing a tree-wide one-off update of the name & style.
    
    Bug: 40136831
    Change-Id: I3ae5f2d1032ec8901acbff2299e68d8b4ec4e96e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/8141242
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>

--- testing_support/fake_repos.py
+++ testing_support/fake_repos.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright 2011 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Generate fake repositories for testing."""
--- tests/gclient_no_sync_smoketest.py
+++ tests/gclient_no_sync_smoketest.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env vpython3
-# Copyright (c) 2022 The Chromium Authors. All rights reserved.
+# Copyright 2022 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Smoke tests for gclient.py and the no-sync experiment
