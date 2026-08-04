d22ef3bf62a8c3c76d9c7427015bdfec7665587a
Alex Ovsienko <ovsienko@google.com>
2026-08-04 04:05:49 -0700

    Ruff: make sure we account for symlinking when resolving parents.
    Bug: 40874143
    Change-Id: I53a079cc7f2efd1abd38bdeab7eabbf96a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/8194065
    Commit-Queue: Alex Ovsienko <ovsienko@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Alex Ovsienko <ovsienko@google.com>

--- tests/ruff_chromium_test.py
+++ tests/ruff_chromium_test.py
@@ -173,6 +173,19 @@ class TestShouldUseRuffRouting(unittest.TestCase):
         self.assertFalse(should_use_ruff("subrepo/foo.py", root_dir=sub_dir))
         self.assertFalse(has_yapf_config("subrepo/foo.py", root_dir=sub_dir))
 
+    def test_root_dir_boundary_stops_traversal_with_symlinks(self):
+        self.write_file("ruff.toml", "")
+        self.write_file(".style.yapf", "")
+        sub_dir = os.path.join(self.test_dir, "subrepo")
+        os.makedirs(sub_dir, exist_ok=True)
+        symlink_sub_dir = os.path.join(self.test_dir, "symlink_subrepo")
+        os.symlink(sub_dir, symlink_sub_dir)
+
+        test_file = os.path.join(symlink_sub_dir, "foo.py")
+        self.assertFalse(should_use_ruff(test_file, root_dir=symlink_sub_dir))
+        self.assertFalse(has_yapf_config(test_file, root_dir=symlink_sub_dir))
+        self.assertEqual("pep8", depot_tools_ruff.get_yapf_style(test_file, root_dir=symlink_sub_dir))
+
     def test_cache_key_depends_on_root_dir(self):
         self.write_file("a/ruff.toml", "")
         sub_dir = os.path.join(self.test_dir, "a/b")
