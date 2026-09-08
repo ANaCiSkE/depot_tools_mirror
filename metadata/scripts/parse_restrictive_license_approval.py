8a7b59a2990b4c12fdb592c9fa24329677074e4f
Jiewei Qian <qjw@chromium.org>
2026-09-07 17:07:58 -0700

    metadata/rla_approval: improve arg passing and error handling
    This CL changes RLA parsing to use env var to pass file paths to avoid
    the need to deal with Windows cmd escaping, and adds exception handling
    to tell the caller that the given textproto is invalid (instead of a
    generic CalledProcessError that eats the important "invalid textproto"
    error message.
    
    Fixed: 558123060
    Change-Id: I202331f00625b5b79d9d8a4419bbc74efb538d24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/8366204
    Reviewed-by: Chenlin Fan <fancl@chromium.org>
    Commit-Queue: Jiewei Qian <qjw@chromium.org>

--- metadata/scripts/parse_restrictive_license_approval.py
+++ metadata/scripts/parse_restrictive_license_approval.py
@@ -21,24 +21,29 @@ sys.path.insert(0, _METADATA_DIR)
 
 import restrictive_license_approval_pb2 as rla_pb2  # noqa: E402
 from google.protobuf import text_format  # noqa: E402
+from fields.custom.license_allowlist import (  # noqa: E402
+    SCRIPT_INPUT_ENV,
+    SCRIPT_EXIT_CODE_INVALID_PROTO,
+)
+
+"""Reads `SCRIPT_INPUT_ENV` environment variable and prints parsing results to stdout."""
 
 
 def main():
-    if len(sys.argv) != 2:
-        print(
-            "Usage: parse_restrictive_license_approval.py <path_to_textproto>",
-            file=sys.stderr,
-        )
-        sys.exit(1)
+    # Don't use sys.argv to pass arguments. Their character escaping is hard
+    # to get right on Windows.
+    path = os.environ.get(SCRIPT_INPUT_ENV, None)
+    if not path:
+        print("No input path given.", file=sys.stderr)
+        sys.exit(2)
 
-    path = sys.argv[1]
     proto_msg = rla_pb2.RestrictiveLicenseApproval()
     try:
         with open(path, "r", encoding="utf-8") as f:
             text_format.Parse(f.read(), proto_msg)
     except Exception as e:
         print(f"Error parsing proto: {e}", file=sys.stderr)
-        sys.exit(2)
+        sys.exit(SCRIPT_EXIT_CODE_INVALID_PROTO)
 
     approvals = []
     for approval in proto_msg.license_approval:
