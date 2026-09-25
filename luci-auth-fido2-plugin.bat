98539f30d9ffc8445490a5a5546e58acfccf92ee
Jiewei Qian <qjw@chromium.org>
2026-09-25 11:23:50 -0700

    luci-auth-fido2-plugin.bat: use abs path to invoke python3
    Fixed: 557837396
    Change-Id: Ib5ea430eefc414f0204fb4c812a9a07deef21cf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/8461032
    Reviewed-by: Gavin Mak <gavinmak@google.com>
    Commit-Queue: Gavin Mak <gavinmak@google.com>
    Auto-Submit: Jiewei Qian <qjw@chromium.org>

--- luci-auth-fido2-plugin.bat
+++ luci-auth-fido2-plugin.bat
@@ -4,9 +4,5 @@
 :: found in the LICENSE file.
 setlocal
 
-:: Ensure that "depot_tools" is somewhere in PATH so this tool can be used
-:: standalone, but allow other PATH manipulations to take priority.
-set PATH=%PATH%;%~dp0
-
-:: Defer control.
-vpython3 "%~dp0\luci_auth_fido2_plugin.py" %*
+call "%~dp0\cipd_bin_setup.bat" > nul 2>&1
+"%~dp0\.cipd_bin\vpython3.exe" "%~dp0\luci_auth_fido2_plugin.py" %*
